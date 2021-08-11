d815eed022d69fe4d3373b51d926bbabd92838f4
muhannadalhariri <37214752+muhannadalhariri@users.noreply.github.com>
2021-08-11 17:24:52 +0300

    Added new line after downloading IPv4 list
    Added new line after downloading IPv4 list 
    Because without the newline \n the last IPv4 and the first IPv6 will be concatenated

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 curl -s https://www.cloudflare.com/ips-v4 -o /tmp/cf_ips
+echo "\n">>/tmp/cf_ips
 curl -s https://www.cloudflare.com/ips-v6 >> /tmp/cf_ips
 
 # Allow all traffic from Cloudflare IPs (no ports restriction)
@@ -16,4 +17,4 @@ ufw reload > /dev/null
 #for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 443 comment 'Cloudflare IP'; done
 
 # Restrict to ports 80 & 443
-#for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 80,443 comment 'Cloudflare IP'; done
+#for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 80,443 comment 'Cloudflare IP'; done                                                                                                                                                                                                                                                                                        
